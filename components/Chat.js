ae9f362e61b3a08955540945c6bd15cc0d7ee7cc
Steven <Briggs.steven369@gmail.com>
2023-04-14 20:37:49 -0700

    comments, add positive messages, set header color

--- components/Chat.js
+++ components/Chat.js
@@ -1,35 +1,89 @@
-import React, {useEffect} from 'react';
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {useEffect, useState} from 'react';
+import {KeyboardAvoidingView, Platform, StyleSheet, View} from 'react-native';
+import {Bubble, GiftedChat} from 'react-native-gifted-chat';
+import {positiveMessages} from '../positiveMessages';
 
 const Chat = ({route, navigation}) => {
+  const [messages, setMessages] = useState([]);
+
+  // save the name and color from the previous screen
+  const name = route.params.name;
+  const color = route.params.color;
+
   useEffect(() => {
-    const name = route.params.name;
-    navigation.setOptions({title: name});
+    //Set the header to use name and color from previous screen, text is set to white to be visible no mater what background color is chosen
+    navigation.setOptions({
+      title: name,
+      headerStyle: {
+        backgroundColor: color
+      },
+      headerTintColor: '#fff'
+    });
+
+    /* Create static messages to display when first opening
+    * The system message will pull from a list of positive messages and use one at random */
+    setMessages([
+      {
+        _id: 1,
+        text: 'Welcome to chat, ' + name,
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'Chat App!',
+          avatar: 'https://picsum.photos/id/237/140/140',
+        },
+      },
+      {
+        _id: 2,
+        text: `Entering chat, remember, ${positiveMessages[Math.floor(Math.random() * positiveMessages.length)]}`,
+        createdAt: new Date(),
+        system: true,
+      },
+    ]);
+
   }, []);
 
-  const name = route.params.name;
-  const backgroundColor = route.params.color;
+  const onSend = (newMessages) => {
+    setMessages(
+      previousMessages => GiftedChat.append(previousMessages, newMessages));
+  };
+
+  // Render the chat bubbles, the users color is matched to the selected color
+  const renderBubble = (props) => {
+    return <Bubble
+      {...props}
+      wrapperStyle={{
+        right: {
+          backgroundColor: color,
+        },
+        left: {
+          backgroundColor: '#FFF',
+        },
+      }}
+    />;
+  };
 
   return (
-    <View style={[styles.chatContainer, {backgroundColor}]}>
-      <Text style={styles.text}>Welcome, {name}!</Text>
-      <TouchableOpacity onPress={() => navigation.navigate('Start')}>
-        <Text style={styles.text}>Go to Start</Text>
-      </TouchableOpacity>
+    <View style={styles.container}>
+      <GiftedChat
+        messages={messages}
+        renderBubble={renderBubble}
+        onSend={messages => onSend(messages)}
+        user={{
+          _id: 1,
+          name,
+        }}
+      />
+      {Platform.OS === 'android'
+        ? <KeyboardAvoidingView behavior="height"/>
+        : null}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  chatContainer: {
+  container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-
-  text: {
-    color: '#FFFFFF',
-    fontSize: 24,
   },
 });
 
