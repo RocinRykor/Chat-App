5d52e3550b71d93f16d1c8d7b9b5845b61cd56db
Steven <Briggs.steven369@gmail.com>
2023-04-29 14:30:47 -0700

    Message caching and offline functionality

--- components/Chat.js
+++ components/Chat.js
@@ -1,6 +1,11 @@
 import {useEffect, useState} from 'react';
 import {KeyboardAvoidingView, Platform, StyleSheet, View} from 'react-native';
-import {Bubble, GiftedChat} from 'react-native-gifted-chat';
+import {
+  Bubble,
+  GiftedChat,
+  InputToolbar,
+  SystemMessage,
+} from 'react-native-gifted-chat';
 import {useNavigation, useRoute} from '@react-navigation/native';
 
 // import firebase functions
@@ -13,13 +18,17 @@ import {
 } from 'firebase/firestore';
 
 
-const Chat = ({db,  isConnected }) => {
+// Importing storage for native apps
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const Chat = ({db, isConnected}) => {
   const [messages, setMessages] = useState([]);
   const route = useRoute();
   const navigation = useNavigation();
 
   // save the name and color from the previous screen
-  const {name, color} = route.params;
+  const {name, color, userID} = route.params;
+  let unsubMessages;
 
   useEffect(() => {
     //Set the header to use name and color from previous screen, text is set to white to be visible no mater what background color is chosen
@@ -31,24 +40,55 @@ const Chat = ({db,  isConnected }) => {
       headerTintColor: '#fff',
     });
 
-    const q = query(collection(db, 'messages'), orderBy('createdAt', 'desc'));
-    const unsubMessages = onSnapshot(q, (documentsSnapshot) => {
-      let newMessages = [];
-      documentsSnapshot.forEach((doc) => {
-        newMessages.push({
-          id: doc.id,
-          ...doc.data(),
-          createdAt: new Date(doc.data().createdAt.toMillis()),
+    if (isConnected === true) {
+      // unregister current onSnapshot() listener to avoid registering multiple listeners when
+      // useEffect code is re-executed.
+      if (unsubMessages) unsubMessages();
+      unsubMessages = null;
+
+      const q = query(collection(db, 'messages'), orderBy('createdAt', 'desc'));
+      unsubMessages = onSnapshot(q, (documentsSnapshot) => {
+        let newMessages = [];
+        documentsSnapshot.forEach((doc) => {
+          newMessages.push({
+            id: doc.id,
+            ...doc.data(),
+            createdAt: new Date(doc.data().createdAt.toMillis()),
+          });
         });
+        cacheMessages(newMessages);
+        setMessages(newMessages);
       });
-      setMessages(newMessages);
-    });
+    } else {
+      loadCachedMessages();
+    }
 
     // code to execute when the component will be unmounted
     return () => {
       if (unsubMessages) unsubMessages();
     };
-  }, []);
+  }, [isConnected]);
+
+
+ // async function that sets messages with cached value
+  // || [] will assign an empty array to cachedMessages if the messages_stored item hasn’t been set yet in AsyncStorage
+  const loadCachedMessages = async () => {
+    const cachedMessages =
+      (await AsyncStorage.getItem('messages')) || [];
+    setMessages(JSON.parse(cachedMessages));
+  };
+
+  // cashing data whenever it is updated
+  const cacheMessages = async (messages) => {
+    try {
+      await AsyncStorage.setItem(
+        'messages',
+        JSON.stringify(messages)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   const onSend = (newMessages) => {
     addDoc(collection(db, 'messages'), newMessages[0]);
@@ -69,13 +109,33 @@ const Chat = ({db,  isConnected }) => {
     />;
   };
 
+  // Disable the input toolbar if not connected
+  const renderInputToolbar = (props) => {
+    if (isConnected) {
+      return <InputToolbar {...props} />;
+    } else {
+      return null;
+    }
+  };
+
+  const renderSystemMessage = (props) => {
+    return (
+      <SystemMessage
+        {...props}
+        textStyle={{fontSize: 14, color: '#fff', fontWeight: 'bold'}}
+      />
+    );
+  };
+
   return (
     <View style={styles.container}>
       <GiftedChat
         messages={messages}
         renderBubble={renderBubble}
+        renderInputToolbar={renderInputToolbar}
+        renderSystemMessage={renderSystemMessage}
         onSend={messages => onSend(messages)}
-        user={{ _id: route.params.userID, username: route.params.name }}
+        user={{_id: userID, username: name}}
       />
       {Platform.OS === 'android'
         ? <KeyboardAvoidingView behavior="height"/>
